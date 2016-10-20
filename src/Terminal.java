d6e2a7641a134ac8f14f11e9f48b3b7a12f6c7f5
seifgamal <seif_gamal09@yahoo.com>
2016-10-20 23:58:34 +0200

    date function has been added

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,9 +1,17 @@
 
-public class Terminal {
+public class CLI {
 
 	/**
 	 * @param args
 	 */
+	private static void date() {
+		// E : for day (Sun Mon Thu ..)
+		// getTime() returns also Time zone like 'EET'
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss E");
+		Calendar cal = Calendar.getInstance();
+		System.out.println(dateFormat.format(cal.getTime()));
+	}
 	public static void main(String[] args) {
 		
 		
