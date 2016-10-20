f05f1804f601fee6cca24793713ff5dc7d8041e7
seifgamal <seif_gamal09@yahoo.com>
2016-10-21 00:54:36 +0200

    function help added

--- src/Terminal.java
+++ src/Terminal.java
@@ -4,6 +4,25 @@ public class CLI {
 	/**
 	 * @param args
 	 */
+	private static String[] validCommands = { "pwd", "ls", "cd", "cp", "mv", "rm", "rmdir", "mkdir", "cat", "more",
+			"date", "clear", "help", "args" };
+	
+	private static String [] Description = {   // to be edit - not important anyway!
+			"display current user directory.",
+			"list each given file or directory name.",
+			"changes the current directory to another one.",
+			"copy two files content cp -r ~/folder1/. ~/new_folder1.", 
+			"renames file SOURCE to DEST, or moves the SOURCE file (or files) to DIRECTORY.",
+			"remove file.",
+			"remove directory",
+			"creates a directory with each given name.",
+			"Concatenate files and print on the standard output.",
+			"display and scroll down the output in one direction only.",
+			"display or set the date and time of the system.",
+			"clear the current terminal screen.",
+			"gives a description of all functions.",
+			"list all parameters on the command line, numbers or strings for specific command."
+	};
 	private static void date() {
 		// E : for day (Sun Mon Thu ..)
 		// getTime() returns also Time zone like 'EET'
@@ -12,13 +31,13 @@ public class CLI {
 		Calendar cal = Calendar.getInstance();
 		System.out.println(dateFormat.format(cal.getTime()));
 	}
-	public static void main(String[] args) {
-		
-		
-		files x = new files();
-		
-		
-		
-				
+	private static void help(String command) { // '?' for all
+		for (int i = 0; i < 14; i++) {
+			if (command == "?" || validCommands[i] == command){
+				System.out.print(validCommands[i]  + ": " + Description[i] + "\n");
+			}
+		}
+	}
+	public static void main(String[] args) {		
 	}	
 }
